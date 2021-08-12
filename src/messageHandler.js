29679fe88bc9ee6deba2b8b0b99e978513e6d0ff
Hilde Vestol <hilde@excolo.dev>
2021-08-12 19:49:35 +0200

    Add a simple command handler for messages

--- src/messageHandler.js
+++ src/messageHandler.js
@@ -1,5 +1,27 @@
 const logger = require('./logger')('messageHandler')
 
+const commandHandler = (message) => {
+  const commands = {
+    ping: () => message.channel.send('pong'),
+    // TODO: Add more commands
+  }
+
+  // The characters immedieatly after ! until we hit a space is the command
+  const commandName = message.content.slice(1).split(' ')[0]
+  const command = commands[commandName]
+
+  // If we find a matching command, then we execute it.
+  if (command) {
+    logger.debug(`Got command "!${commandName}" and executing`)
+    command()
+  }
+}
+
 module.exports = (message) => {
-  logger.info('message received:', message.content)
+  // A message is considered a command if it starts with !
+  const isCommand = message.content[0] === '!'
+
+  if (isCommand) commandHandler(message)
+
+  logger.info(`message received: "${message.content}"`)
 }
