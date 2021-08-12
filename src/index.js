0384f33abcb2fc2cacaaa31bab124a23134be572
Hilde Vestol <hilde@excolo.dev>
2021-08-12 18:47:11 +0200

    Extract messageHandler

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
 const { Client } = require('discord.js')
+const messageHandler = require('./messageHandler')
 
 const client = new Client()
 
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`))
-client.on('message', (message) =>
-  console.log('message received:', message.content)
-)
+client.on('message', messageHandler)
 
 client.login(process.env.TOKEN)
